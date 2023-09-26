015bd42db5d1802af80f3b99b6d7e105b1b2ea21
vikaspatil2277 <vikaspatil2277@gmail.com>
2023-09-26 13:15:13 +0530

    modified

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const express=require("express");
-// const categoryRouter=require("../route/categoryRouter");
-const categoryRouter = require('./route/categoryRouter');
-const cors=require("cors")
-
-
 const app=express();
-app.use(express.json())
+const categoryRouter = require('./route/categoryRouter');
+const cors=require("cors");
+
+
+app.use(express.json());
 
 //CORS middleware to allow cross-origin access from the frontend server (http://localhost:4000
 app.use(cors({
     origin:"*" 
-}))
-app.use("/api/categories",categoryRouter)
+}));
+app.use("/api/categories",categoryRouter);
 
 
 app.listen(4000,()=>{
     console.log('Server is running on port 4000');
-})
+});
